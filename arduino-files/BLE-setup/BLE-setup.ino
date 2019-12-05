b8807abd24a4ba385507aab21e9b4f5ef0cd74b5
atharvapatil <atharvaabhaypatil@gmail.com>
2019-12-05 18:07:19 -0500

    one char continious valur reading

--- arduino-files/BLE-setup/BLE-setup.ino
+++ arduino-files/BLE-setup/BLE-setup.ino
@@ -1,59 +1,56 @@
 #include <SPI.h>
 // Using the ArduinoBLE library for Bluetooth communication by advertising Arduino as a discoverable peripheral
 #include <ArduinoBLE.h>
+#include <Arduino_LSM9DS1.h>
 
 BLEService punchService("19b10010-e8f2-537e-4f6c-d104768a1214");
 
-BLEByteCharacteristic videoScrubCharacteristic("6635d693-9ad2-408e-ad48-4d8f88810dee", BLERead | BLENotify);
-//BLEDescriptor videoScrubDescriptor("2901", "Video Scrub");
-BLEByteCharacteristic audioScrubCharacteristic("099af204-5811-4a15-8ffb-4f127ffdfcd7", BLERead | BLENotify);
-//BLEDescriptor audioScrubDescriptor("2901", "Volume");
+BLEByteCharacteristic punchCharacteristic("6635d693-9ad2-408e-ad48-4d8f88810dee", BLERead | BLENotify);
 
-const int videoScrubPin = A3;   // to scrub the video based on the chain position on the apron
-const int audioScrubPin = A4;   // to control the audio of the iOS device.
-
-
+int POT_THRESHOLD = 50;
 
 void setup() {
   // put your setup code here, to run once:
 
   Serial.println("Setup started...");
 
-    pinMode(videoScrubPin, INPUT); // use the slide potentiometer as a input
-  pinMode(audioScrubPin, INPUT); // use the slide potentiometer as a input
+  Serial.begin(9600);
+  while (!Serial);
 
-    // begin initialization
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+
+
+  // begin initialization
   if (!BLE.begin()) {
     Serial.println("starting BLE failed!");
     //    while (1);
   } else {
 
-    BLE.setLocalName("Punch Rev");
+    BLE.setLocalName("Punch");
 
     Serial.println("Local name set...");
 
-  // set the UUID for the service this peripheral advertises:
-  BLE.setAdvertisedService(punchService);
+    // set the UUID for the service this peripheral advertises:
+    BLE.setAdvertisedService(punchService);
 
-  punchService.addCharacteristic(videoScrubCharacteristic);
-//  videoScrubCharacteristic.addDescriptor(videoScrubDescriptor);
-  punchService.addCharacteristic(audioScrubCharacteristic);
-//  audioScrubCharacteristic.addDescriptor(audioScrubDescriptor);
+    punchService.addCharacteristic(punchCharacteristic);
 
 
-  // add the service
-  BLE.addService(punchService);
+    // add the service
+    BLE.addService(punchService);
 
-  Serial.println("Service added...");
+    Serial.println("Service added...");
 
 
-  videoScrubCharacteristic.writeValue(100);
-  audioScrubCharacteristic.writeValue(99);
+    punchCharacteristic.writeValue(100);
 
     // start advertising
-  BLE.advertise();
+    BLE.advertise();
 
-  Serial.println("Bluetooth device active, waiting for connections...");
+    Serial.println("Bluetooth device active, waiting for connections...");
 
   }
 
@@ -61,8 +58,36 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  float aX, aY, aZ, gX, gY, gZ;
 
   // poll for BLE events
   BLE.poll();
 
+    // check if both new acceleration and gyroscope data is
+    // available
+    if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()) {
+      // read the acceleration and gyroscope data
+      IMU.readAcceleration(aX, aY, aZ);
+      IMU.readGyroscope(gX, gY, gZ);
+
+      // print the data in CSV format
+      Serial.println(aX*100, 3);
+
+      int videoScrubState = aX*100;
+
+        if (videoScrubValueChanged(videoScrubState)) {
+//      Serial.print("Video Scrub ");
+//      Serial.println(videoScrubState);
+      punchCharacteristic.writeValue(videoScrubState);
+  }
+    }
+
+
+}
+
+boolean videoScrubValueChanged(int videoScrub) {
+  return (
+    (videoScrub < (punchCharacteristic.value() - POT_THRESHOLD)) ||
+    (videoScrub > (punchCharacteristic.value() + POT_THRESHOLD))
+  );
 }
